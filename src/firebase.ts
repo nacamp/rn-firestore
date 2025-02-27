1b2872c58774572119bb787c7901882d1a4a4641
jimmy <najimmy@gmail.com>
2025-02-27 13:27:48 +0900

    Separate config by platform

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,8 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
-
-// Firebase 설정 타입 정의
+import { firebaseEnv} from "./config";
 interface FirebaseConfig {
   apiKey: string;
   authDomain: string;
@@ -15,17 +14,7 @@ interface FirebaseConfig {
   databaseURL: string;
 }
 
-// 환경변수에서 Firebase 설정 가져오기
-const firebaseConfig: FirebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID as string,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID as string | undefined,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL as string,
-};
+const firebaseConfig: FirebaseConfig = firebaseEnv;
 
 // Firebase 초기화
 const app = initializeApp(firebaseConfig);
